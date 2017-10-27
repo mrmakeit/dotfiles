44d92f11bad6f2dfeb003aac3fcb30b370a7f52b
Blixa Morgan <blixa@projectmakeit.com>
2017-10-27 01:04:38 -0500

    Update mutt to support smtp, add killterm command, add mutt to work tmuxinator

--- offlineimap.py
+++ offlineimap.py
@@ -3,8 +3,7 @@ import subprocess
 
 def mailpasswd(acct):
   acct = os.path.basename(acct)
-  path = "/home/blixa/.password-store/%s.gpg" % acct
-  args = ["gpg2", "--use-agent", "--quiet", "--batch", "-d", path]
+  args = ["pass", "show", acct]
   try:
     return subprocess.check_output(args).strip()
   except subprocess.CalledProcessError:
